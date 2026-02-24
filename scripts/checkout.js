60353ee412b1b813b94d81bc5996987242a78bfe
Renuka Patil <renukapatil627@gmail.com>
2026-02-24 21:35:53 +0530

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,9 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadFromStorage } from "../data/cart.js"; // ← ADD THIS
+import { loadFromStorage } from "../data/cart.js";
+import "../data/backend-practice.js";
 
-loadFromStorage(); // ← ADD THIS
+loadFromStorage();
 
 renderOrderSummary();
 renderPaymentSummary();
