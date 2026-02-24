869976b7160e7ed5ff821e63682afc9469b406d8
Renuka Patil <renukapatil627@gmail.com>
2026-02-24 21:59:39 +0530

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,11 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
 import { loadFromStorage } from "../data/cart.js";
-import "../data/backend-practice.js";
+// import "../data/backend-practice.js";
 
 loadFromStorage();
-
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
