c0a8dc9f7d9e6df18f332685eadd59bb7fcda171
Renuka Patil <renukapatil627@gmail.com>
2026-02-25 19:30:03 +0530

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,10 +2,52 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 import { loadFromStorage } from "../data/cart.js";
+import { loadCart } from "../data/cart.js";
 // import "../data/backend-practice.js";
 
-loadFromStorage();
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve("value1");
+    });
+  }),
+  new Promise((resolve) => {
+    loadFromStorage();
+    loadCart(() => {
+      resolve("hi");
+    });
+  }),
+]).then((values) => {
+  console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*new Promise((resolve) => {
+  loadProducts(() => {
+    resolve("value1");
+  });
+})
+  .then((value) => {
+    console.log(value);
+
+    return new Promise((resolve) => {
+      loadFromStorage();
+      loadCart(() => {
+        resolve();
+      });
+    });
+  })
+  .then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });*/
+
+/*
+loadFromStorage();
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});*/
